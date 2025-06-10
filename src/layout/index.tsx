d18900f3e16c683500e7c6111f44ec24a572cc1e
Đỗ Anh Sơn <doanhson20082003@gmail.com>
2025-06-10 23:32:02 +0700

    feat: create layout template

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,6 +1,16 @@
-import AppProviders from "@/providers/AppProviders";
-import { Outlet } from "@tanstack/react-router";
+import { Outlet } from '@tanstack/react-router'
+import { HeaderComponent } from './header'
+import { SidebarComponent } from './sidebar'
+import { Fragment } from 'react'
 
 export default function Layout() {
-  return <Outlet/>
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <HeaderComponent />
+      <div className="flex mx-auto">
+        <SidebarComponent />
+        <Outlet />
+      </div>
+    </div>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
-import { queryClient } from './lib/queryClient'
 import { routeTree } from './routeTree.gen'
 import AppProviders from './providers/AppProviders'
 
-const router = createRouter({ routeTree })
+export const router = createRouter({ routeTree, scrollRestoration: true })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
