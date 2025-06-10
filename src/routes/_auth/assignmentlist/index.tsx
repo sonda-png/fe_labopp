e3567fc5dbb8f209584bf362761df92119aeca22
dungpmhe170403 <dungpmhe170403@fpt.edu.vn>
2025-06-10 23:42:49 +0700

    feat/hotfix

---
+++ src/routes/_auth/assignmentlist/index.tsx
@@ -0,0 +1,7 @@
+import { createFileRoute } from '@tanstack/react-router'
+import LabAssignmentList from '@/pages/lab_assignment_list'
+export const Route = createFileRoute('/_auth/assignmentlist/')({     
+  component: LabAssignmentList,
+})
+
+
---
+++ src/routes/_auth/list-lab/index.tsx
@@ -0,0 +1,7 @@
+import { createFileRoute } from '@tanstack/react-router'
+import ListLab from '@/pages/listlab'
+export const Route = createFileRoute('/_auth/list-lab/')({
+  component: ListLab,
+})
+
+
