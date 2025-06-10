0200ef500870487446f16190e32037a2f93c7169
Đỗ Anh Sơn <doanhson20082003@gmail.com>
2025-06-10 23:32:02 +0700

    feat: create layout template

--- src/routes/_public/home/index.tsx
+++ src/routes/_public/home/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import Home from '../../../pages/home'
+import About from '@/pages/about'
 
 export const Route = createFileRoute('/_public/home/')({
-  component: Home,
+  component: About,
 })
