0340d8d3de2f0f92d7d21eae636d5267e7aa7228
Đỗ Anh Sơn <214675135+sonda-png@users.noreply.github.com>
2025-07-01 18:10:40 +0700

    feat(manage-semester): add crud manage semester

---
+++ src/api/actions/teacher-assignment/teacher-assignment.mutations.ts
@@ -0,0 +1,9 @@
+import { AxiosInstance } from "axios";
+
+export const semestersMutations = {
+    createSemesterClass: (client: AxiosInstance) => handleCreateSemesterClass(client),
+}
+
+const handleCreateSemesterClass = (client: AxiosInstance) => async (body: CreateSemesterRequest) => {
+    return (await client.post<Semester>('/head_subject/semester/class', body)).data
+}
--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -22,8 +22,13 @@ import { authStore } from '@/stores/authStore'
 import { useNavigate } from '@tanstack/react-router'
 
 export const HeaderComponent = () => {
-  const { authValues } = authStore()
+  const { authValues, clearTokens } = authStore()
   const navigate = useNavigate()
+
+  const handleLogout = () => {
+    clearTokens()
+    navigate({ to: '/login' })
+  }
   return (
     <header className="bg-white border-b border-gray-200 px-6 py-4 sticky top-0 z-50">
       <div className="flex items-center justify-between  mx-auto">
@@ -71,18 +76,13 @@ export const HeaderComponent = () => {
                     className="flex items-center gap-3 text-gray-900 px-2 py-1 rounded-full hover:bg-orange-50 transition"
                   >
                     <Avatar className="h-8 w-8">
-                      <AvatarImage
-                        src={
-                          authValues.image ||
-                          '/placeholder.svg?height=32&width=32'
-                        }
-                      />
+                      <AvatarImage />
                       <AvatarFallback className="bg-orange-500 text-white">
-                        {authValues.userName?.[0] || 'U'}
+                        {authValues.email?.[0] || 'U'}
                       </AvatarFallback>
                     </Avatar>
                     <div className="text-left hidden md:block">
-                      <div className="font-medium">{authValues.userName}</div>
+                      <div className="font-medium">{authValues.email}</div>
                       <div className="text-xs text-gray-500">
                         {authValues.email}
                       </div>
@@ -103,11 +103,7 @@ export const HeaderComponent = () => {
                     Cài đặt hệ thống
                   </DropdownMenuItem>
                   <Separator />
-                  <DropdownMenuItem
-                    onClick={() => {
-                      navigate({ to: '/login' })
-                    }}
-                  >
+                  <DropdownMenuItem onClick={handleLogout}>
                     <LogOut className="mr-2 h-4 w-4" />
                     Đăng xuất
                   </DropdownMenuItem>
